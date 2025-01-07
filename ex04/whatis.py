8d88743d3615f7c85fc936b62d87f93b885e4f84
jeelee <>
2025-01-07 23:31:40 +0900

    Refactor: It is norm!(flake8)

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -5,16 +5,16 @@ argv = sys.argv
 argc = len(argv)
 
 try:
-  assert argc == 2, "more than one argument is provided"
+    assert argc == 2, "more than one argument is provided"
 
-  value = argv[1]
+    value = argv[1]
 
-  try:
-    if int(value) % 2 == 0:
-      print("I'm Even.")
-    else:
-      print("I'm Odd.")
-  except ValueError as e:
-    raise AssertionError("argument is not an integer")
+    try:
+        if int(value) % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+    except ValueError:
+        raise AssertionError("argument is not an integer")
 except AssertionError as e:
-  print(f"AssertionError: {e}")
+    print(f"AssertionError: {e}")
